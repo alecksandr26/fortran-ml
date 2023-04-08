54185228f053ebcdf86cb66d43200ae7c268abdb
alecksandr <sansepiol26@gmail.com>
2023-04-08 01:35:06 -0600

    Its working but needs more trainning

--- examples/perceptron_rectangle_circle/src/config.h
+++ examples/perceptron_rectangle_circle/src/config.h
@@ -3,8 +3,7 @@
 
 #define WIDTH 20
 #define HEIGHT 20
-#define CHECK_SEED 420
-#define SAMPLE_SIZE 75
+#define SAMPLE_SIZE 1000
 #define PPM_SCALER 25
 
 #endif
--- examples/perceptron_rectangle_circle/src/main.f90
+++ examples/perceptron_rectangle_circle/src/main.f90
@@ -39,34 +39,35 @@ program p_rectangle_circle
 
     matrix = 0
     ! Catch all the training examples
-    call prepare_training_data(train_inputs, train_outputs)
+    call prepare_data(train_inputs, train_outputs, "train")
 
     ! Train the perceptron
-
-    call p_train(per, train_inputs, train_outputs, 0.01, 100)
+    call p_train(per, train_inputs, train_outputs, 0.01, 100000)
 
     ! Test the perceptron
-    call p_test(per, train_inputs, results)
+    call prepare_data(test_inputs, test_outputs, "test")
 
+    call p_test(per, test_inputs, results)
+
+    sum = 0
     do i = 1, SAMPLE_SIZE
-        if (train_outputs(i) == results(i)) sum = sum + 1
+        if (results(i) == test_outputs(i)) sum = sum + 1
     end do
 
-    ! write(*, "(f3.3), (a)") (sum / real(SAMPLE_SIZE)) * 100.0, " %"
-    print *, (sum / int(SAMPLE_SIZE)) * 100.0
-    print *, train_outputs(1:10)
-    print *, results(1:10)
+    ! Print the results
+    print *, (sum / real(SAMPLE_SIZE)) * 100.0, sum, SAMPLE_SIZE
     
     call p_free(per)
     
 contains
 
-    ! prepare_test_data: To test the prepare data
-    subroutine prepare_test_data(test_inputs, test_outputs)
-        real(real32), intent(in out) :: test_inputs(:, :)
-        real(real32), intent(in out) :: test_outputs(:)
+    ! prepare_data: Generates random inputs and outputs for the treanning
+    subroutine prepare_data(inputs, outputs, prefix)
+        real(real32), intent(in out) :: inputs(:, :)
+        real(real32), intent(in out) :: outputs(:)
+        character(len = *), intent(in) :: prefix
         
-        character(len = 20) :: file_path
+        character(len = 50) :: file_path
         real(real32) :: mat_for_sample(WIDTH, HEIGHT) ! The used matrix for each examplen
         integer(int32) :: i, cor, circle_c, rect_c
 
@@ -80,57 +81,21 @@ contains
 
             if (cor == 1) then  ! rectangle
                 call mat_random_rect(mat_for_sample)
-                write(file_path, "(a, i0, a)") "data/test_rect-", rect_c, ".ppm"
+                write(file_path, "(a, i0, a)") "data/" // prefix // "rect-", rect_c, ".ppm"
                 rect_c = rect_c + 1
             else
                 call mat_random_circle(mat_for_sample)
-                write(file_path, "(a, i0, a)") "data/test_circle-", circle_c, ".ppm"
+                write(file_path, "(a, i0, a)") "data/" // prefix // "circle-", circle_c, ".ppm"
                 circle_c = circle_c + 1
             endif
             
             
             ! Catch the generated data
-            test_inputs(i, :) = reshape(mat_for_sample, (/WIDTH * HEIGHT/))
-            test_outputs(i) = cor - 1
+            inputs(i, :) = reshape(mat_for_sample, (/WIDTH * HEIGHT/))
+            outputs(i) = cor - 1
 
             ! Write the matrix
             call mat_save_as_ppm(mat_for_sample, file_path)
         end do
-    end subroutine prepare_test_data
-    
-    ! prepare_training_data: Sets all randomly all the data
-    subroutine prepare_training_data(train_inputs, train_outputs)
-        real(real32), intent(in out) :: train_inputs(:, :) 
-        real(real32), intent(in out) ::  train_outputs(:)
-        
-        character(len = 20) :: file_path
-        real(real32) :: mat_for_sample(WIDTH, HEIGHT) ! The used matrix for each examplen
-        integer(int32) :: i, cor, circle_c, rect_c
-        
-        rect_c = 0
-        circle_c = 0
-        
-        ! Iterate each sample and generate        
-        do i = 1, SAMPLE_SIZE
-            cor = random_range(1, 2)
-            call mat_fill_rect(mat_for_sample, 0, 0, WIDTH, HEIGHT, 0.0)
-            
-            if (cor == 1) then  ! rectangle
-                call mat_random_rect(mat_for_sample)
-                write(file_path, "(a, i0, a)") "data/rect-", rect_c, ".ppm"
-                rect_c = rect_c + 1
-            else
-                call mat_random_circle(mat_for_sample)
-                write(file_path, "(a, i0, a)") "data/circle-", circle_c, ".ppm"
-                circle_c = circle_c + 1
-            endif
-
-            ! Catch the generated data
-            train_inputs(i, :) = reshape(mat_for_sample, (/WIDTH * HEIGHT/))
-            train_outputs(i) = cor - 1
-            
-            ! Write the matrix
-            call mat_save_as_ppm(mat_for_sample, file_path)
-        end do
-    end subroutine prepare_training_data
+    end subroutine prepare_data
 end program p_rectangle_circle
